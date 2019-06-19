8264ea4d07428d318f149aa1f402a934ea4c9094
Ruonan Li <liruonan@hotmail.com>
2019-06-19 10:16:11 +0800

    up

--- AmountFormat.js
+++ AmountFormat.js
@@ -1,7 +1,7 @@
 
 var _sv = (function (global, factory, plug) {
-    return global[plug] = factory.call(global);
-})(this, function () {
+    return global[plug] = factory.call(global, plug);
+})(this, function (plug) {
     return function (num, fixed = 2) {
         if (typeof num !== 'number') return num;
 
@@ -11,14 +11,20 @@ var _sv = (function (global, factory, plug) {
             [strNo, strDecimal] = strNum.split('.');
 
         if (strNum.includes('e')) return num;
-        return `${num < 0 ? '-' : ''}${formatInt(strNo.replace('-', ''))}.${strDecimal || '00'}`;
+
+        const sv = `${num < 0 ? '-' : ''}${formatInt(strNo.replace('-', ''))}.${strDecimal || '00'}`;
+        return sv;
     };
 
+    function formatInt(no) {
+        // return formatInt_v1(no);
+        return formatInt_v2(no);
+    }
     /**
      * 整数格式化
      * @param {Int} no 
      */
-    function formatInt(no) {
+    function formatInt_v1(no) {
         if ((no = no + '').length < 4) return no;
 
         const array = no.split(''), int_count = array.length / 3;
@@ -27,5 +33,21 @@ var _sv = (function (global, factory, plug) {
             array[index] = ',' + array[index]
         }
         return array.join('');
+    }
+
+    /**
+     * 整数格式化
+     * @param {Int} no 
+     */
+    function formatInt_v2(no) {
+        if ((no = no + '').length < 4) return no;
+
+        let result = '';
+        for (let index = no.length - 1; index >= 0; index--) {
+            if (result.length % 3 === 0 && index !== 0) result = ',' + result;
+            result = no.charAt(index) + result;
+        }
+
+        return result;
     }
 }, 'AmountFormat');
--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 sv-cases
---------
+=========
 认真小例
 
 GestrueScale
-=============
+-------------
 手势缩放
 
 [Online_Demo_Is_Here](./GestureScale/docs/pages/index/index.html)
 
 
 AmountFormat 金额格式化
-========================
+------------------------
 [Online_Demo_Is_Here](./test/amountFormat.html)
